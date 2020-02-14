dce82c45b7a66d42ab3ca3527c0ff65e3965a4c1
hakim-21 <61012972+hakim-21@users.noreply.github.com>
2020-02-14 09:28:14 +0700

    Delete ready-to-git.py

--- ready-to-git.py
+++
@@ -1,6 +0,0 @@
-# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
-print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
-print("1. Moh Noor Al Azam (04060302)")
-print("2. Achmad Musyaffa Taufiqi (043129023)")
-print("3. Bhaskara Anugrah Pramada (043129027)")
-print("Muhammad Husnul Hakim(04117022)")
